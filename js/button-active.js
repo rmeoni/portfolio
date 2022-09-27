65c562a7330d50d9986f2c31a35bc352335bb28f
UX Rodrigo <rodrigo.me13@gmail.com>
2022-09-26 19:23:01 -0600

    fixing classes for active button

--- js/button-active.js
+++ js/button-active.js
@@ -7,8 +7,8 @@ var btns = btnContainer.getElementsByClassName("tag-badge");
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    var current = document.getElementsByClassName("activeBtn");
+    current[0].className = current[0].className.replace(" activeBtn", "");
+    this.className += " activeBtn";
   });
 }
