de60335e1d0e251cc2891deb3035b1c890bda60e
UX Rodrigo <rodrigo.me13@gmail.com>
2022-09-26 18:20:50 -0600

    adding new works to portfolio

---
+++ js/projects.js
@@ -0,0 +1,238 @@
+const projects = {
+    ukp: {
+        title: 'Unsere Kleine Potfarm UI Design',
+        category: 'Prototype Design',
+        description: 'During this project I got to work on the <strong>Research, Design and Ideation</strong> process of a cannabis educational blog focused on the german market. Since the legalization of medicinal cannabis in the EU is slowly happenning, this startup seeks to inform german patients about the in-and-outs of the cannabis world.',
+        link: 'https://www.figma.com/proto/VkEcXX513HG6Eyrupg7Tg1/UI-KIT-UKP?page-id=0%3A1&node-id=833%3A2955&viewport=1126%2C2233%2C0.33&scaling=scale-down-width&starting-point-node-id=833%3A2955',
+        image: 'images/portfolio-thumb-8.jpeg',
+        buttonText: 'View Prototype',
+    },
+    eagles: {
+        title: "Eagle's Nest UI Design",
+        category: 'UI Design',
+        description: "For this project I had the opportunity to work along with a team of 8+ creatives. Including marketing specialists, copywriters, programmers and team leaders. By sticking to the brand's core beliefs and values I worked developing a <strong>Design System and Prototype in Figma. </strong> Afterwards I handed off the <strong>CSS</strong> to the developers. While I did some coding as well, most of the development of the site was taken care by another programmer to divide labor and separate concerns. The site was built in <strong>Wordpress.</strong>",
+        link: 'https://eaglesnestatitlan.com/',
+        image: 'images/portfolio-thumb-4.jpeg',
+        buttonText: 'Visit Website',
+    },
+    utopia : {
+        title: 'Utopia UI Design',
+        category: 'UI Design',
+        description: 'During this project I got the opportunity to work alongside a Dutch designer and developer. It was an eye-opening experience for I got to learn the basics of wireframing with <strong>Sketch</strong> as well as to strenghten my web development skills. Even thou this project was done using <strong>Shopify</strong>, I got the Javascript bug thanks to this master developer I got to work with. I got a first-hand opportunity to see an app being built from scratch using node.js as well as to see the design process of it.',
+        link: 'https://utopia-eco-hotel.myshopify.com/',
+        image: 'images/portfolio-thumb-6.jpeg',
+        buttonText: 'Visit Website',
+    },
+    windhillgrowers: {
+        title: "Wind Hill Growers Design System",
+        category: "UI Design",
+        description: "Working in the expansion of the website from this beautiful brand we got to build a <strong>Design System in Figma</strong> to keep all our digital products on brand. I've been lucky enough to be able to collaborate and keep expanding this website. Built on <strong>Squarespace</strong>",
+        link: "https://www.windhillgrowers.com/",
+        image: "images/portfolio-thumb-10.jpeg",
+        buttonText: 'Visit Website',
+    },
+    xejas: {
+        title: "Restaurant Paseo Xejasmin Booking Page",
+        category: "UI Design",
+        description: "One of my first coding projects. For this awesome place I got to build a solution from scratch using <strong>HTML, CSS and Basic Javascript</strong>. This website showcases the attractions from the place, allows customers to download a PDF menu trough a QR code and even book events that go into <strong>Podio CRM</strong> directly from the home page. ",
+        link: "https://www.paseoxejasmin.com/",
+        image: "images/portfolio-thumb-5.jpeg",
+        buttonText: 'Visit Website',
+    },
+    santelmo: {
+        title: "Restaurant San Telmo Landing Page",
+        category: "UI Design",
+        description: "This project was coded from scratch using <strong>HTML, CSS and Javascript.</strong> The vision of the project was to create a formidable landing page with a satisfactory user flow. Making all the different platforms from the business accesible from the main menu, thus increasing the conversion into bookings and sales.",
+        link: "https://www.santelmoflores.com/",
+        image: "images/portfolio-thumb-3.jpg",
+        buttonText: 'Visit Website',
+    },
+    whgapp: {
+        title: "Cannabis Tester App",
+        category: "Javascript",
+        description: "This Web Application was done from scratch. I designed the UI in <strong>Figma</strong>, and then developed the site with <strong>HTML, CSS and Javascript.</strong> The app takes the users answers to provide a personalized product reocommendation of the Wind Hill Growers products. This was a whole bunch of Vanilla Javascript fun!",
+        link: "https://try.windhillgrowers.com/",
+        image: "images/portfolio-thumb-1.png",
+        buttonText: 'Visit Webapp',
+    },
+    cafedonlucas: {
+        title: "Coffee Brand UX Research/Design",
+        category: "UX Design",
+        description: "In this project I got to use the Design Thinking approach. Following the 'Emphatize, Define, Ideate, Prototype and Test' methodology, we started by conducting <strong>Qualitative and Quantitative Research</strong> to define our <strong>User Personas</strong>, needs and painpoints. Afterwards we Ideated with the help of the <strong>User Journey Map</strong> to come up with possible solutions to solve our user painpoints. We followed the process by creating a <strong>Wireframe and Prototype with Figma </strong> and ended by conducting Usability Tests to the design. ",
+        link: "https://www.figma.com/proto/0Df7gWKIRrghwUBLCBZQvU/Caf%C3%A9-Don-Lucas-Prototype?page-id=0%3A1&node-id=2%3A96&viewport=338%2C290%2C0.11&scaling=scale-down-width&starting-point-node-id=2%3A96",
+        image: "images/portfolio-thumb-7.jpeg",
+        buttonText: 'View Prototype',
+
+    },
+    wht: {
+        title: "Wind Hill Tours UX Design",
+        category: "UX Design",
+        description: "For this culturally rich project I got to work with a team of creatives focusing on the Colombian market. We performed <strong>Quantitative an Qualitative research </strong>, collecting data trough surveys and statistics to better understand our audience. Then we designed a <strong>Prototype in Figma</strong> putting our user's painpoints first. Then we developed the website on in <strong>Squarespace</strong> providing a great customer experience by solving our users problems in an accesible way.  ",
+        link: "https://windhilltours.com/",
+        image: "images/portfolio-thumb-9.jpeg",
+        buttonText: 'Visit Website',
+    },
+    lozana : {
+        title: 'Lozana Prototype Design',
+        category: 'Prototype Design',
+        description: 'In this opportunity I got to design the User Interface of a Real State company. Working hand in hand with the client, we designed a <strong>prototype with Figma</strong> to then be <strong>developed in Webflow.</strong> ',
+        link: 'https://www.figma.com/proto/8X8SJLP7QarDud2e1hDE00/Lozana-Web-Design?page-id=164%3A1650&node-id=164%3A1779&viewport=25%2C72%2C0.08&scaling=scale-down-width&starting-point-node-id=164%3A1779',
+        image: './images/portfolio-thumb-2.jpg',
+        buttonText: 'View Prototype',
+    },
+}
+
+
+
+
+const renderProjects = (projects,category) => {
+
+    let projectsDiv = document.getElementById('portfolio-project');
+    projectsDiv.innerHTML = "";
+
+    let workGrid = document.getElementById('works-grid-1');
+    let workGrid2 = document.getElementById('works-grid-2');
+    workGrid2.innerHTML = "";
+
+
+    projectsObj = Object.entries(projects);
+    console.log(projectsObj);
+
+    let filteredProjects = projectsObj.filter(project => project[1].category === category);
+  
+
+
+//if it shows 2 projects or less
+    if(filteredProjects.length === 2 ) {
+        workGrid.style.gridRowGap = '0px';
+        for(let i = 0; i < filteredProjects.length; i++){
+
+            if(i===1) {
+                let project = document.createElement('div');
+            project.setAttribute('class',`projects ${category}`);
+          
+            project.innerHTML = 
+           
+            `<a href="${filteredProjects[i][1].link}" target="_blank" class="work-card-preview w-inline-block">
+                <div class="card-hover-circle"></div>
+                <div class="overlay" style="display: none; transform: translate3d(150%, -150%, 0px) scale3d(2, 2, 1) rotateX(0deg) rotateY(0deg) rotateZ(45deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+                <img src="${filteredProjects[i][1].image}" alt="" class="bg-image absolute" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+            </a>
+            <h4> ${filteredProjects[i][1].title} </h4>
+            <div class="work-card-description">
+                <p> ${filteredProjects[i][1].description} </p>
+            </div>
+            
+            <a href="${filteredProjects[i][1].link}" target="_blank" class="portfolio-links">
+            ${filteredProjects[i][1].buttonText}
+            </a>
+            `;
+            
+            projectsDiv.appendChild(project);
+            } else {
+                //add a new grid for the 3rd+ projects
+
+                let project = document.createElement('div');
+            project.setAttribute('class',`projects ${category}`);
+          
+            project.innerHTML = 
+           
+            `<a href="${filteredProjects[i][1].link}" target="_blank" class="work-card-preview w-inline-block">
+                <div class="card-hover-circle"></div>
+                <div class="overlay" style="display: none; transform: translate3d(150%, -150%, 0px) scale3d(2, 2, 1) rotateX(0deg) rotateY(0deg) rotateZ(45deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+                <img src="${filteredProjects[i][1].image}" alt="" class="bg-image absolute" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+            </a>
+            <h4> ${filteredProjects[i][1].title} </h4>
+            <div class="work-card-description">
+                <p> ${filteredProjects[i][1].description} </p>
+            </div>
+            
+            <a href="${filteredProjects[i][1].link}" target="_blank" class="portfolio-links">
+            ${filteredProjects[i][1].buttonText}
+            </a>
+            `;
+            
+            workGrid2.appendChild(project);
+            }
+        }
+    }else if (filteredProjects.length >= 3){
+        for(let i = 0; i < filteredProjects.length; i++){
+
+            if(i<2) {
+                let project = document.createElement('div');
+            project.setAttribute('class',`projects ${category}`);
+          
+            project.innerHTML = 
+           
+            `<a href="${filteredProjects[i][1].link}" target="_blank" class="work-card-preview w-inline-block">
+                <div class="card-hover-circle"></div>
+                <div class="overlay" style="display: none; transform: translate3d(150%, -150%, 0px) scale3d(2, 2, 1) rotateX(0deg) rotateY(0deg) rotateZ(45deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+                <img src="${filteredProjects[i][1].image}" alt="" class="bg-image absolute" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+            </a>
+            <h4> ${filteredProjects[i][1].title} </h4>
+            <div class="work-card-description">
+                <p> ${filteredProjects[i][1].description} </p>
+            </div>
+            
+            <a href="${filteredProjects[i][1].link}" target="_blank" class="portfolio-links">
+            ${filteredProjects[i][1].buttonText}
+            </a>
+            `;
+            
+            projectsDiv.appendChild(project);
+            } else {
+                //add a new grid for the 3rd+ projects
+
+                let project = document.createElement('div');
+            project.setAttribute('class',`projects ${category}`);
+          
+            project.innerHTML = 
+           
+            `<a href="${filteredProjects[i][1].link}" target="_blank" class="work-card-preview w-inline-block">
+                <div class="card-hover-circle"></div>
+                <div class="overlay" style="display: none; transform: translate3d(150%, -150%, 0px) scale3d(2, 2, 1) rotateX(0deg) rotateY(0deg) rotateZ(45deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+                <img src="${filteredProjects[i][1].image}" alt="" class="bg-image absolute" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+            </a>
+            <h4> ${filteredProjects[i][1].title} </h4>
+            <div class="work-card-description">
+                <p> ${filteredProjects[i][1].description} </p>
+            </div>
+            
+            <a href="${filteredProjects[i][1].link}" target="_blank" class="portfolio-links">
+            ${filteredProjects[i][1].buttonText}
+            </a>
+            `;
+            
+            workGrid2.appendChild(project);
+            }
+        }
+
+    }else {
+        //if it shows 3 or more projects
+        for(let i = 0; i < filteredProjects.length; i++){
+            let project = document.createElement('div');
+            project.setAttribute('class',`projects ${category}`);
+          
+            project.innerHTML = 
+           
+            `<a href="${filteredProjects[i][1].link}" target="_blank" class="work-card-preview w-inline-block">
+                <div class="card-hover-circle"></div>
+                <div class="overlay" style="display: none; transform: translate3d(150%, -150%, 0px) scale3d(2, 2, 1) rotateX(0deg) rotateY(0deg) rotateZ(45deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+                <img src="${filteredProjects[i][1].image}" alt="" class="bg-image absolute" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+            </a>
+            <h4> ${filteredProjects[i][1].title} </h4>
+            <div class="work-card-description">
+                <p> ${filteredProjects[i][1].description} </p>
+            </div>
+            
+            <a href="${filteredProjects[i][1].link}" target="_blank" class="portfolio-links">
+            ${filteredProjects[i][1].buttonText}
+            </a>
+            `;
+            
+            projectsDiv.appendChild(project);
+        }
+    }
+}
+
+renderProjects(projects, "UI Design");
