d4a6fcbba9fb7c1ee5dc6640257f1af9d98822d9
UX Rodrigo <rodrigo.me13@gmail.com>
2024-09-24 22:46:19 +0200

    Updating portfolio

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,10 @@
 const projects = {
-    ukp: {
-        title: 'Unsere Kleine Potfarm UI Design',
+    mwd: {
+        title: 'Mit Was Drin',
         category: 'Prototype Design',
-        description: 'During this project I got to work on the <strong>Research, Design and Ideation</strong> process of a cannabis educational blog focused on the german market. Since the legalization of medicinal cannabis in the EU is slowly happenning, this startup seeks to inform german patients about the in-and-outs of the cannabis world.',
-        link: 'https://www.figma.com/proto/VkEcXX513HG6Eyrupg7Tg1/UI-KIT-UKP?page-id=0%3A1&node-id=833%3A2955&viewport=1126%2C2233%2C0.33&scaling=scale-down-width&starting-point-node-id=833%3A2955',
-        image: 'images/portfolio-thumb-8.jpeg',
+        description: 'In this project, I worked on the Research, Design, and Ideation for a cannabis education blog targeting the German market. With the gradual legalization of medicinal cannabis in the EU, the startup aims to inform German patients about the essentials of the cannabis industry.',
+        link: 'https://xd.adobe.com/view/93568bae-b1c5-4629-82e5-928b5d19f3fa-c605/',
+        image: 'images/portfolio-thumb-8.png',
         buttonText: 'View Prototype',
     },
     xejas: {
@@ -12,7 +12,7 @@ const projects = {
         category: "UI Design",
         description: "One of my first coding projects. For this awesome place I got to build a solution from scratch using <strong>HTML, CSS and Basic Javascript</strong>. This website showcases the attractions from the place, allows customers to download a PDF menu trough a QR code and even book events that go into <strong>Podio CRM</strong> directly from the home page. ",
         link: "https://www.paseoxejasmin.com/",
-        image: "images/portfolio-thumb-5.jpeg",
+        image: "images/portfolio-thumb-5.png",
         buttonText: 'Visit Website',
     },
     windhillgrowers: {
@@ -20,13 +20,13 @@ const projects = {
         category: "UI Design",
         description: "Working in the expansion of the website from this beautiful brand we got to build a <strong>Design System in Figma</strong> to keep all our digital products on brand. I've been lucky enough to be able to collaborate and keep expanding this website. Built on <strong>Squarespace</strong>",
         link: "https://www.windhillgrowers.com/",
-        image: "images/portfolio-thumb-10.jpeg",
+        image: "images/portfolio-thumb-10.png",
         buttonText: 'Visit Website',
     },
     whgapp: {
         title: "Cannabis Tester App",
         category: "Javascript",
-        description: "This Web Application was done from scratch. I designed the UI in <strong>Figma</strong>, and then developed the site with <strong>HTML, CSS and Javascript.</strong> The app takes the users answers to provide a personalized product reocommendation of the Wind Hill Growers products. This was a whole bunch of Vanilla Javascript fun!",
+        description: "I designed and developed this web application from scratch. The UI was created in Figma, and I built the site using HTML, CSS, and JavaScript. The app uses user inputs to deliver personalized product recommendations for Wind Hill Growers. It was a fun project, packed with Vanilla JavaScript!This Web Application was done from scratch. I designed the UI in <strong>Figma</strong>, and then developed the site with <strong>HTML, CSS and Javascript.</strong> The app takes the users answers to provide a personalized product reocommendation of the Wind Hill Growers products. This was a whole bunch of Vanilla Javascript fun!",
         link: "https://try.windhillgrowers.com/",
         image: "images/portfolio-thumb-1.png",
         buttonText: 'Visit Webapp',
@@ -34,9 +34,9 @@ const projects = {
     cafedonlucas: {
         title: "Coffee Brand UX Research/Design",
         category: "UX Design",
-        description: "In this project I got to use the Design Thinking approach. Following the 'Emphatize, Define, Ideate, Prototype and Test' methodology, we started by conducting <strong>Qualitative and Quantitative Research</strong> to define our <strong>User Personas</strong>, needs and painpoints. Afterwards we Ideated with the help of the <strong>User Journey Map</strong> to come up with possible solutions to solve our user painpoints. We followed the process by creating a <strong>Wireframe and Prototype with Figma </strong> and ended by conducting Usability Tests to the design. ",
+        description: "In this project, I used the Design Thinking approach, starting with Qualitative and Quantitative Research to define User Personas and identify pain points. We then used a User Journey Map to ideate solutions, followed by creating a Wireframe and Prototype in Figma. Finally, we conducted Usability Testing to refine the design based on user feedback.",
         link: "https://www.figma.com/proto/0Df7gWKIRrghwUBLCBZQvU/Caf%C3%A9-Don-Lucas-Prototype?page-id=0%3A1&node-id=2%3A96&viewport=338%2C290%2C0.11&scaling=scale-down-width&starting-point-node-id=2%3A96",
-        image: "images/portfolio-thumb-7.jpeg",
+        image: "images/portfolio-thumb-7.png",
         buttonText: 'View Prototype',
 
     },
@@ -45,7 +45,7 @@ const projects = {
         category: 'Prototype Design',
         description: 'In this opportunity I got to design the User Interface of a Real Estate company. Working hand in hand with the client, we designed a <strong>prototype with Figma</strong> to then be <strong>developed in Webflow.</strong> ',
         link: 'https://www.figma.com/proto/8X8SJLP7QarDud2e1hDE00/Lozana-Web-Design?page-id=164%3A1650&node-id=164%3A1779&viewport=25%2C72%2C0.08&scaling=scale-down-width&starting-point-node-id=164%3A1779',
-        image: './images/portfolio-thumb-2.jpg',
+        image: './images/portfolio-thumb-2.png',
         buttonText: 'View Prototype',
     },
     gummies_mockups: {
@@ -77,7 +77,7 @@ const projects = {
         category: 'Javascript',
         description: 'A company was struggling with managing inventory in Zoho Inventory. I created a JavaScript that pulls new invoices via the Zoho Inventory API and pushes them to Google Sheets, allowing them to easily manage the data.',
         link: './detail_project_zoho_google_api.html',
-        image: './images/project_zoho_inventory-to-google-sheets-api.png',
+        image: './images/portfolio-thumb-17.png',
         buttonText: 'View Project',
     },
 }
@@ -233,4 +233,4 @@ const renderProjects = (projects, category) => {
     }
 }
 
-renderProjects(projects, "UI Design");
+renderProjects(projects, "UX Design");
